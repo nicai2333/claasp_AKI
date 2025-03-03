9b1e985ca8416eaeb1c683dd5c556f7830cbf486
nicai2333 <134794315+nicai2333@users.noreply.github.com>
2025-03-03 15:24:15 +0800

    klein_block_cipher.py

--- klein_block_cipher.py
+++ klein_block_cipher.py
@@ -1,93 +1,117 @@
-import random
-import secrets
-import numpy as np
-import math as math
-from enum import Enum
-from operator import xor
-from copy import deepcopy
+# ****************************************************************************
+# Copyright 2023 Technology Innovation Institute
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ****************************************************************************
+
+
 from claasp.cipher import Cipher
-from claasp import editor 
+from claasp.utils.utils import get_ith_word
+from claasp.DTOs.component_state import ComponentState
+from claasp.name_mappings import INPUT_PLAINTEXT, INPUT_KEY
 
-# 生成加密安全的128位无符号整数
-def generate_random_number(size):
-    random_bytes = secrets.token_bytes(size//8)
-    return int.from_bytes(random_bytes, byteorder='big')
+input_types = [INPUT_KEY, INPUT_PLAINTEXT]
+PARAMETERS_CONFIGURATION_LIST = [{'block_bit_size': 64, 'key_bit_size': 64, 'number_of_rounds': 12}]
 
-def bitwise_xor(a, b):
-    return (a | b) - (a & b)
 
-class AKI:
-
+class KleinBlockCipher(Cipher):
     """
+    Construct an instance of the KleinBlockCipher class.
+
+    This class is used to store compact representations of a cipher,
+    used to generate the corresponding cipher.
+
     INPUT:
 
-    - ``cipher`` -- the cipher to be computed, define the number of the round in advance
-    - ``word_size`` -- the word_size of the cipher
-    - ``number_of_rounds`` -- **integer**  The number of rounds where the intermediate status value resides 
-    - ``the_position`` -- **integer** The position of the intermediate status value in the round
+    - ``block_bit_size`` -- **integer** (default: `64`); cipher input and output block bit size of the cipher
+    - ``key_bit_size`` -- **integer** (default: `64`); cipher key bit size of the cipher, and both number of 80 and 96 are avaliable 
+    - ``number_of_rounds`` -- **integer** (default: `12`); number of rounds of the cipher. When key_bit_size is 80 , the number of 
+    rounds of the cipher is 16. When key_bite_size is 96, the number of rounds of the cipher is 20. 
 
-    sage: from claasp.ciphers.block_ciphers.speck_block_cipher import SpeckBlockCipher
-    sage: from claasp.cipher_modules.statistical_tests.AKI import AKI
-    sage: path = AKI(SpeckBlockCipher(number_of_rounds=3))
-    sage: result = path.computate_path_generate(the_position=3,word_size=16)
+    EXAMPLES::
+        sage: from claasp.ciphers.block_ciphers.klein_block_cipher import KleinBlockCipher
+        sage: klein=KleinBlockCipher()
+        sage: plaintext = 0xffffffffffffffff
+        sage: key = 0x0000000000000000
+        sage: klein.evaluate([plaintext,key])
+        14826049118395313086
+        sage: hex(klein.evaluate([plaintext,key]))
+        '0xcdc0b51f14722bbe'
+    """
 
-    sage: from claasp.ciphers.block_ciphers.aes_block_cipher import AESBlockCipher
-    sage: from claasp.cipher_modules.statistical_tests.AKI import AKI
-    sage: path = AKI(AESBlockCipher(number_of_rounds=3))
-    sage: path.computate_path_generate(the_position=127,word_size=8)
+    def __init__(self, number_of_rounds=12, key_bit_size=64):
+        self.block_bit_size = 64
+        self.key_bit_size = key_bit_size
+        self.WORD_SIZE = 4
+        self.SBOX = [0x7,0x4,0xa,0x9,0x1,0xf,0xb,0x0,0xc,0x3,0x2,0x6,0x8,0xe,0xd,0x5]
+                       
+        super().__init__(family_name="klein",
+                         cipher_type="block_cipher",
+                         cipher_inputs=[INPUT_PLAINTEXT, INPUT_KEY],
+                         cipher_inputs_bit_size=[self.block_bit_size, self.key_bit_size],
+                         cipher_output_bit_size=self.block_bit_size)
 
-    Since the short round aes in claasp is composed of the whitening key XOR and
-    the last round of encryption without mixcolumns, the difference on one byte in the aes_inverse propagates one more round
+        state = INPUT_PLAINTEXT
+        key = INPUT_KEY
 
-
-    """    
-    
-    def __init__(self, cipher):
-        self.cipher = cipher
-        str_of_inputs_bit_size = list(map(str, cipher.inputs_bit_size))
-        self._cipher_primitive = cipher.id + "_" + "_".join(str_of_inputs_bit_size)
-
-    def computate_path_generate(self, the_position, word_size):
-
-        if(self.cipher.inputs[0]=='plaintext') :
-            block_size=self.cipher.inputs_bit_size[0]
-            key_size = self.cipher.inputs_bit_size[1]
-        else :
-            block_size=self.cipher.inputs_bit_size[1]
-            key_size = self.cipher.inputs_bit_size[0]
+        if(number_of_rounds>12):
+            if (key_bit_size==64): 
+                raise ValueError("number_of_rounds incorrect (should be less then or equal to 12)")
+            elif (key_bit_size==80 and number_of_rounds>16):   
+                raise ValueError("number_of_rounds incorrect (should be less then or equal to 16)")
+            elif (key_bit_size==96 and number_of_rounds>20):   
+                raise ValueError("number_of_rounds incorrect (should be less then or equal to 20)")
         
-        KEY = generate_random_number(key_size)
+        for round_i in range(1, number_of_rounds + 1):
+            self.add_round()
+            round_key = self.add_round_key_output_component([key], [list(range(64))], 64).id  #
+            state = self.round_function(state, round_key)  
+            key = self.update_key(key, round_i)
+        round_key = self.add_round_key_output_component([key], [list(range(64))], 64).id
+        state = self.add_XOR_component([state, round_key], [list(range(64))] + [list(range(64))], 64).id
+        self.add_cipher_output_component([state], [list(range(64))], 64)
 
-        zero_block_size = 0
-        mask = 1 << the_position
-        begin = zero_block_size | mask
-        cipher_inv = self.cipher.cipher_inverse()
+    def update_key(self, k, i):
+        long = self.key_bit_size//2
+        rot_k_1 = self.add_rotate_component([k],[list(range(long))],long,-8).id
+        rot_k_2 = self.add_rotate_component([k],[list(range(long,self.key_bit_size))],long,-8).id
+        xor0 = self.add_XOR_component([rot_k_1, rot_k_2], [list(range(long))]+[list(range(long))], long).id
+        const = self.add_constant_component(8,i).id
+        xor1 = self.add_XOR_component([rot_k_2,const],[list(range(16,24))]+[list(range(8))],8).id
+        left = self.add_concatenate_component([rot_k_2,xor1,rot_k_2],[list(range(16)),list(range(8)),list(range(24,long))],long).id
+        s0 = self.add_SBOX_component([xor0], [list(range(8,12))], 4, self.SBOX).id  #
+        s1 = self.add_SBOX_component([xor0], [list(range(12,16))], 4, self.SBOX).id  #
+        s2 = self.add_SBOX_component([xor0], [list(range(16,20))], 4, self.SBOX).id  #
+        s3 = self.add_SBOX_component([xor0], [list(range(20,24))], 4, self.SBOX).id  #
+        right = self.add_concatenate_component([xor0,s0,s1,s2,s3,xor0],[list(range(8)),list(range(4)),list(range(4)),
+                                                                        list(range(4)),list(range(4)),list(range(24,long))],long).id
+        updated_key = self.add_intermediate_output_component([left,right],
+                                                             [list(range(long)),list(range(long))], self.key_bit_size, 'updated_key')
+        return updated_key.id
 
-        if (cipher_inv.inputs[1]=='key'): 
-            result1=cipher_inv.evaluate([zero_block_size,KEY],intermediate_output=True)
-            result2=cipher_inv.evaluate([begin,KEY],intermediate_output=True)
-        else:
-            result1=cipher_inv.evaluate([KEY,zero_block_size],intermediate_output=True)
-            result2=cipher_inv.evaluate([KEY,begin],intermediate_output=True)
-        result3 = []
-        for i in range(self.cipher.number_of_rounds-1):
-            temp = bitwise_xor (result1[1]['round_output'][i],result2[1]['round_output'][i])
-            result3.append(temp)
-        result3.append(bitwise_xor (result1[1]['plaintext'][0],result2[1]['plaintext'][0]))
+    def round_function(self, x, k):
+        after_key_add = self.add_XOR_component([x, k], [list(range(64))] + [list(range(64))], 64).id
+        sb_outputs = [self.add_SBOX_component([after_key_add], [list(range(i*4, (i+1)* 4))], 4,
+                                              self.SBOX).id for i in range(16)]
+        left_word_rotated = self.add_rotate_component(sb_outputs, [list(range(4)) for i in range(16)], 64, -16).id
+        mix_columns_1 = self.add_mix_column_component([left_word_rotated],[list(range(32))],32,
+                                                      [[[0x02, 0x03, 0x01, 0x01], [0x01, 0x02, 0x03, 0x01], [0x01, 0x01, 0x02, 0x03],
+                     [0x03, 0x01, 0x01, 0x02]],283,8]).id
+        mix_columns_2 = self.add_mix_column_component([left_word_rotated],[list(range(32,64))],32,
+                                                      [[[0x02, 0x03, 0x01, 0x01], [0x01, 0x02, 0x03, 0x01], [0x01, 0x01, 0x02, 0x03],
+                     [0x03, 0x01, 0x01, 0x02]],283,8]).id
 
-        binary_list = []
-        for num in result3:
-            binary_list.append( format(num, '0%db' %(block_size)))
-
-        result4 = []
-        for j in range(len(binary_list)):
-            for i in range(0, len(binary_list[j]), word_size):
-                if ((binary_list[j][i:i+word_size])!='0'*word_size):
-                    result4.append('K_%d_%d_%d' %(len(binary_list)-j-1,(i//word_size)//4,(i//word_size)%4))
-        
-        return result4
-        
-
-        
-    
+        round_output = self.add_round_output_component([mix_columns_1, mix_columns_2], [list(range(32)), list(range(32))], 64).id
+        return round_output
     
